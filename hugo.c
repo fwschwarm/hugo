cc52226a1077c891bacafed012cdc1bdf9e69e75
Fritz-Walter Schwarm <fritz.schwarm@sternwarte.uni-erlangen.de>
2016-05-19 20:25:34 +0200

    kind of working version

--- hugo.c
+++ hugo.c
@@ -15,15 +15,36 @@
 #define SERVO_LEFT      1
 #define SERVO_RIGHT     0
  
-#define INPUT_DDR       DDRC
-#define INPUT_PORT      PORTC
-#define INPUT_PIN       PINC
-#define INPUT_GROUND    2
-
+#define SENSOR_DDR	DDRC
+#define SENSOR_PORT	PORTC
+#define SENSOR_PIN	PINC
+#define SENSOR_LEFT	1
+#define SENSOR_RIGHT	0
+#define SENSOR_GROUND	2
 
 //ISR(TIMER0_OVF_vect)
 //{
 //}
+
+//! Read value from ADC
+uint16_t measure(uint8_t ch)
+{
+	//! Select ADC Channel ch must be 0-7
+	ch = ch&0b00000111;
+	ADMUX &= ch;
+	ADMUX |= ch;
+
+	//! Start Single conversion
+	ADCSRA |= (1<<ADSC);
+
+	//! Wait for conversion to complete
+	while (!(ADCSRA & (1<<ADIF)));
+
+	//! Clear ADIF
+	ADCSRA |= (1<<ADIF);
+
+	return(ADC);
+}
 
 //! Move from -90 to 90 degree
 //! Offset goes from -1 to 1.0
@@ -31,18 +52,18 @@ void move(int id, double offset)
 {
 	switch (id) {
 	case SERVO_LEFT:
-		if (offset == 0.0) {
-			OCR1A = -1;
-		} else {
+//		if (offset == 0.0) {
+//			OCR1A = -1;
+//		} else {
 			OCR1A = ICR1 * (1.5 + 0.5 * offset) / 20;
-		}
+//		}
 		break;
 	case SERVO_RIGHT:
-		if (offset == 0.0) {
-			OCR1B = -1;
-		} else {
+//		if (offset == 0.0) {
+//			OCR1B = -1;
+//		} else {
 			OCR1B = ICR1 * (1.5 - 0.5 * offset) / 20;
-		}
+//		}
 		break;
 	}
 }
@@ -51,28 +72,35 @@ int init(void)
 {
 	int i;
 
-	// Enable ADC
-	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);
-//	ADMUX = 0x00;
-	ADMUX = (1<<REFS0);
-
 	SERVO_PORT = 0x00;
 	SERVO_DDR = 0xFF;                     
 
+	//! Activate the servo hardware timers
 	ICR1 = 19999;
 	TCCR1A  = (1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);
 	TCCR1B =  (1<<WGM13)|(1<<WGM12)|(1<<CS10);
 
-	// Configure debouncing routines
-	INPUT_DDR &= ~0xFF;	// configure key port for input
-	INPUT_PORT |= 0xFF;	// and turn on pull up resistors
- 
+
+	//! Configure sensor port
+	SENSOR_DDR &= ~0xFF;
+	//! Turn on internal pull ups
+	SENSOR_PORT |= 0xFF;
+
+	//! Enable the ADC
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS0);
+	ADMUX = (1<<REFS0);
+//ADMUX=(1<<REFS0);                         // For Aref=AVcc;
+//ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0); //Rrescalar div factor =128
+
+	//! Activate the 8-bit timer
 	TCCR0 = (1<<CS02)|(1<<CS00);         // divide by 1024
 //	TCNT0 = (uint8_t)(int16_t)-(F_CPU / 1024 * 10e-3 + 0.5);  // preload for 10ms
 //	TIMSK |= 1<<TOIE0;                   // enable timer interrupt
 
+	//! Enable interupts
 	sei();
 
+	//! Stop motors
 	move(SERVO_LEFT, 0.0);
 	move(SERVO_RIGHT, 0.0);
 }
@@ -88,12 +116,25 @@ int main( void )
 //		while (ADCSRA & (1<<ADSC));
 //		g_speed = ADCW;
 
-		if (INPUT_PIN &= 1<<INPUT_GROUND) {
-			move(SERVO_LEFT, 0.0);
-			move(SERVO_RIGHT, 0.0);
+		if (SENSOR_PIN &= 1<<SENSOR_GROUND) {
+
+			if (measure(SENSOR_LEFT) > 512)
+				move(SERVO_LEFT, 0.1);
+			else
+				move(SERVO_LEFT, 0.0);
+
+
+			if (measure(SENSOR_RIGHT) > 512)
+				move(SERVO_RIGHT, 0.1);
+			else
+				move(SERVO_RIGHT, 0.0);
+
 		} else {
-			move(SERVO_LEFT, -0.1);
-			move(SERVO_RIGHT, -0.1);
+			if (measure(SENSOR_LEFT) < measure(SENSOR_RIGHT)) {
+				move(SERVO_LEFT, -0.1);
+			} else {
+				move(SERVO_RIGHT, -0.1);
+			}
 		}
 
 	}
